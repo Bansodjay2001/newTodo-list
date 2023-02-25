3630cebdaa2302db279be60cce5970335acfdc04
Aditya <adityabansod001@gmail.com>
2023-02-25 13:48:17 +0530

    first commit

---
+++ modern-todo-list/src/App.jsx
@@ -0,0 +1,11 @@
+ import React from "react";
+ import TodoList from "./assets/components/TodoList";
+import "./App.css"
+
+ export default function App(){
+    return (
+        <div>
+            <TodoList />
+        </div>
+    )
+ }
